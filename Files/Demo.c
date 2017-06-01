05a6bfde2e436f5e5e73e214b46ab35fe45e5f63
Efthymios Koktsidis <efthymios-ks@users.noreply.github.com>
2017-06-01 16:59:54 +0300

    Update Demo.c

--- Files/Demo.c
+++ Files/Demo.c
@@ -5,7 +5,6 @@
 int main(void)
 {
 	//Variables
-	uint8_t status = DHT_OK;	//Return status of sensor
 	double temp[1], hum[1];		//Return values of sensor - Must be pointers or more preferably size-one arrays
 	temp[0] = hum[0] = 0;
 
@@ -21,21 +20,21 @@ int main(void)
 		status = DHT_status();
 		
 		//Check status
-		if (status == DHT_OK)
+		switch (DHT_STATUS)
 		{
-			//Print temperature
-			//print(temp[0]);
+			case (DHT_OK):
+				//Print temperature
+				//print(temp[0]);
 	
-			//Print humidity
-			//print(hum[0]);
-		}
-		else if (status == DHT_ERROR_CHECKSUM)
-		{
-			//Do something
-		}
-		else if (status == DHT_ERROR_TIMEOUT)
-		{
-			//Do something else
+				//Print humidity
+				//print(hum[0]);	
+				break;
+			case (DHT_ERROR_CHECKSUM):
+				//Do something
+				break;
+			case (DHT_ERROR_TIMEOUT):
+				//Do something else
+				break;
 		}
 				
 		//Sensor needs 1-2s to stabilize its readings
